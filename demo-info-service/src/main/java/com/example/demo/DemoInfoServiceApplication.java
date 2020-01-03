fc7f9b31e26dd44fa0db569d17044cb6d4a0eee3
ansar <sayed.ansarali@gmail.com>
2020-01-03 10:43:04 +0530

    initial commit

---
+++ demo-info-service/src/main/java/com/example/demo/DemoInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoInfoServiceApplication.class, args);
+	}
+
+}
